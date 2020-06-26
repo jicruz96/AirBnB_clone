7ccba83d6eabbdaa4a97959266095a3257c8ce32
jicruz96 <accounts@jicruz.com>
2020-06-26 23:44:39 +0000

    Add test file templates

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Contains unittests for BaseModel class """
+import unittest
+import os
+from models.base_model import BaseModel
+
+
+class TestBaseModelClass(unittest.TestCase):
+    """ Tests BaseModel class """
+
+    def test_init(self):
+        """ Tests init method """
+
+    def test_str(self):
+        """ Tests __str__ method """
+
+    def test_save(self):
+        """ Tests save method """
+
+    def test_to_dict(self):
+        """ Tests to_dict method """
---
+++ tests/test_models/test_user.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Contains unittests for User class """
+import unittest
+import os
+from models.user import User
+
+
+class TestUserClass(unittest.TestCase):
+    """ Tests User class """
