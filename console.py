246029c49da0512b1490aa0daa855e0bb0dfcc1a
Kelsie Merchant <eislek02@gmail.com>
2020-06-30 20:50:15 -0500

    Add console methods to control objects through FileStorage and BaseMethod methods; pep8 checks and tested locally in terminal

--- console.py
+++ console.py
@@ -4,6 +4,8 @@ Creating command interpreter
 """
 import cmd
 import sys
+from models.engine.known_objects import classes
+from models import storage
 
 
 # Ensure it works in both interactive and non-interactive mode
@@ -18,109 +20,186 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, arg):
         """ EOF command to exit the program """
-        # Figure this out
-        if sys.stdout.isatty():
-            print('')
+        if sys.stdin.isatty() is False:
+            print("")
         return True
 
     def emptyline(self):
         """ Does nothing, re-prompts """
         pass
 
-    def do_create(self, cls_name=None):
+    def do_create(self, arguments):
         """
-        Creates a new instance of BaseModel
+        Creates a new instance of a class
         saves it (to the JSON file)
         and prints the id.
             Ex: $ create BaseModel
         """
-
-        if self.not_a_class(cls_name):
+        args = self.arg_string_parse(arguments)
+        if self.not_a_class(args["cls_name"]):
             return
-        # Create instance of class
-        obj = cls_name()
-        # Save to JSON File
-        obj.save()
-        # Print the ID
-        print(obj.id)
+        cls = classes[args["cls_name"]]
+        new_obj = cls()
+        new_obj.save()
+        print(new_obj.id)
 
-    def do_show(self, cls_name=None, id=None):
+    def do_show(self, arguments):
         """
-        Prints the string representation of an instance based on the class name and id.
+        Prints the string representation of an instance
+        based on the class name and id.
         Ex: $ show BaseModel 1234-1234-1234.
         """
-        if self.not_a_class(cls_name):
+        args = self.arg_string_parse(arguments)
+        if self.not_a_class(args["cls_name"]):
             return
-        if self.not_an_instance(id):
+        if self.not_an_instance(args["cls_name"], args["inst_id"]):
             return
-        # cls_name.id.__str__()
+        key = "{}.{}".format(args["cls_name"], args["inst_id"])
+        __objects = storage.all()
+        print(__objects[key])
 
-    def do_destroy(self, cls_name=None, id=None):
+    def do_destroy(self, arguments):
         """ Deletes an instance based on the class name and id
         (save the change into the JSON file).
         Ex: $ destroy BaseModel 1234-1234-1234.
         """
-        if self.not_a_class(cls_name):
+        args = self.arg_string_parse(arguments)
+        if self.not_a_class(args["cls_name"]):
             return
-        if self.not_an_instance(id):
+        if self.not_an_instance(args["cls_name"], args["inst_id"]):
             return
-        # Use storage.something to delete
-        # Make sure you "save changes" to JSON file
+        key = "{}.{}".format(args["cls_name"], args["inst_id"])
+        __objects = storage.all()
+        del __objects[key]
+        storage.save()
 
-    def do_all(self, cls_name=None):
+    def do_all(self, arguments):
         """
-        Prints all string representation of all instances based on the class name or not.
+        Prints all string representation of all instances
+        based on the class name or not.
         Ex: $ all BaseModel or $ all.
         """
-        if cls_name is None:
-            pass
-            # catch and print all - loop through all, printing __str__
-        if self.not_a_class(cls_name):
-            return
-        # loop through all in class name, printing __str__
+        args = self.arg_string_parse(arguments)
+        __objects = storage.all()
+        print_list = []
+        if args["cls_name"] is None:
+            for obj in __objects:
+                print_string = "{}".format(__objects[obj].__str__())
+                print_list.append(print_string)
+        else:
+            if self.not_a_class(args["cls_name"]):
+                return
+            for obj in __objects:
+                if obj.split(".")[0] == args["cls_name"]:
+                    print_string = "{}".format(__objects[obj].__str__())
+                    print_list.append(print_string)
+        print(print_list)
 
-    def do_update(self, cls_name=None, id=None, attr=None, attr_value=None):
+    def do_update(self, arguments):
         """
         Updates an instance based on the class name and id
         adds or updates attribute and saves the change into the JSON file
-        Ex: $ update BaseModel 1234-1234-1234 email "aibnb@holbertonschool.com".
+        Ex: update BaseModel 1234-1234-1234 email "aibnb@holbertonschool.com".
         """
-        if self.not_a_class(cls_name):
+        args = self.arg_string_parse(arguments)
+        if self.not_a_class(args["cls_name"]):
             return
-        if self.not_an_instance(id):
+        if self.not_an_instance(args["cls_name"], args["inst_id"]):
             return
-        if attr is None:
-            print("** attribute name missing **")
+        if self.not_an_attribute(args["attr_name"]):
             return
-        if attr_value is None:
-            print("** value missing **")
+        if self.not_a_value(args["attr_value"]):
             return
-        # set attr_value to attr for cls_name.id
+        key = "{}.{}".format(args["cls_name"], args["inst_id"])
+        __objects = storage.all()
+        setattr(__objects[key], args["attr_name"], args["attr_value"])
+
+    @staticmethod
+    def arg_string_parse(arguments):
+        """
+        Splits input string of arguments
+        arg order determines type of argument
+        and arguments are space delimited
+
+        Expected order:
+        0. class name
+        1. instance id
+        2. attribute name
+        3. attribute value
+        """
+        arg_list = arguments.split()
+        args = {}
+        if len(arg_list) >= 1:
+            args["cls_name"] = arg_list[0]
+        else:
+            args["cls_name"] = None
+        if len(arg_list) >= 2:
+            args["inst_id"] = arg_list[1]
+        else:
+            args["inst_id"] = None
+        if len(arg_list) >= 3:
+            args["attr_name"] = arg_list[2]
+        else:
+            args["attr_name"] = None
+        if len(arg_list) >= 4:
+            args["attr_value"] = arg_list[3]
+        else:
+            args["attr_value"] = None
+        return args
 
     @staticmethod
     def not_a_class(cls_name):
+        """
+        Checks if given valid class name argument
+        Prints error message if missing or invalid
+        """
         if cls_name is None:
             print("** class name missing **")
             return True
-
-        from models.engine.storage import known_classes
-
-        classes = known_classes()
-
-        if cls_name not in known_classes.:
+        if cls_name not in classes:
             print("** class doesn't exist **")
             return True
-
         return False
 
     @staticmethod
-    def not_an_instance(cls_name, id):
-        if id is None:
+    def not_an_instance(cls_name, inst_id):
+        """
+        Checks if given valid instance id argument
+        Prints error message if missing or invalid
+        """
+        if inst_id is None:
             print("** instance id missing **")
             return True
-        key = "{}.{}".format(cls_name, id)
-        if key not in storage.all():
+        key = "{}.{}".format(cls_name, inst_id)
+        __objects = storage.all()
+        if key not in __objects:
             print("** no instance found **")
+            return True
+        return False
+
+    @staticmethod
+    def not_an_attribute(attr_name):
+        """
+        Checks if given attribute name argument
+        Prints error message if missing
+        If exists, attr_name is assumed to be valid
+            for this model
+        """
+        if attr_name is None:
+            print("** attribute name missing **")
+            return True
+        return False
+
+    @staticmethod
+    def not_a_value(attr_value):
+        """
+        Checks if given attribute value argument
+        Prints error message if missing
+        If exists, attr_valid is assumed to be valid
+            for this model, but may require type casting
+        """
+        if attr_value is None:
+            print("** value missing **")
             return True
         return False
 
