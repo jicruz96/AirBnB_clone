4fa5770ae74ac2da8891506890b51967adff8339
Kelsie Merchant <eislek02@gmail.com>
2020-07-01 11:08:40 -0500

    Add unittests for all() and new() methods

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,6 +3,7 @@
 import unittest
 import os
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 
 class TestFileStorageClass(unittest.TestCase):
@@ -10,9 +11,40 @@ class TestFileStorageClass(unittest.TestCase):
 
     def test_all(self):
         """ Tests all method """
+        # create storage instance and instance of BaseModel
+        storage = FileStorage()
+        obj = BaseModel()
+        __objects = storage.all()
+        # test that storage.all() returns dictionary
+        self.assertIsInstance(__objects, dict)
+        # test that BaseModel instance in dictionary of objects
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        self.assertIn(key, __objects)
+        # test that the value in dictionary is of same type and equal to obj
+        self.assertEqual(obj, __objects[key])
+        self.assertIsInstance(__objects[key], BaseModel)
 
     def test_new(self):
         """ Tests new method """
+        # create storage instance and instance of BaseModel
+        storage = FileStorage()
+        obj = BaseModel()
+        # create dictionary to set obj values to with **kwargs
+        new_dict = {}
+        new_dict["id"] = "012345"
+        new_dict["created_at"] = "1995-2-2T10:23:35.123450"
+        new_dict["updated_at"] = "1999-1-4T7:15:05.543210"
+        # create instance with **kwargs and test that object is not in __objects
+        obj2 = BaseModel(**new_dict)
+        key = "{}.{}".format(type(obj2).__name__, obj2.id)
+        self.assertNotIn(key, storage.all())
+        # obj2 is reset to new instance of BaseModel and
+        # should be added to dictionary with new
+        obj2 = BaseModel()
+        key = "{}.{}".format(type(obj2).__name__, obj2.id)
+        __objects = storage.all()
+        self.assertIn(key, __objects)
+        self.assertEqual(obj2, __objects[key])
 
     def test_save(self):
         """ Tests save method """
